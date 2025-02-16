f795c4ba5d97f0c92c9b413f308a5de069846f94
Kavindu Senesh <ksenesh5@gmail.com>
2025-02-16 21:21:06 +0530

    refactor: restore addToLikedMovies function with improved error handling

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -12,29 +12,6 @@ module.exports.getLikedMovies = async (req, res) => {
     }
 };
 
-module.exports.addToLikedMovies = async (req, res) => {
-    try {
-        const { email, data } = req.body;
-        const user = await await User.findOne({ email });
-        if (user) {
-            const { likedMovies } = user;
-            const movieAlreadyLiked = likedMovies.find(({ id }) => id === data.id);
-            if (!movieAlreadyLiked) {
-                await User.findByIdAndUpdate(
-                    user._id,
-                    {
-                        likedMovies: [...user.likedMovies, data],
-                    },
-                    { new: true }
-                );
-            } else return res.json({ msg: "Movie already added to the liked list." });
-        } else await User.create({ email, likedMovies: [data] });
-        return res.json({ msg: "Movie successfully added to liked list." });
-    } catch (error) {
-        return res.json({ msg: "Error adding movie to the liked list" });
-    }
-};
-
 module.exports.removeFromLikedMovies = async (req, res) => {
     try {
         const { email, movieId } = req.body;
@@ -57,5 +34,28 @@ module.exports.removeFromLikedMovies = async (req, res) => {
         } else return res.json({ msg: "User with given email not found." });
     } catch (error) {
         return res.json({ msg: "Error removing movie to the liked list" });
+    }
+};
+
+module.exports.addToLikedMovies = async (req, res) => {
+    try {
+        const { email, data } = req.body;
+        const user = await await User.findOne({ email });
+        if (user) {
+            const { likedMovies } = user;
+            const movieAlreadyLiked = likedMovies.find(({ id }) => id === data.id);
+            if (!movieAlreadyLiked) {
+                await User.findByIdAndUpdate(
+                    user._id,
+                    {
+                        likedMovies: [...user.likedMovies, data],
+                    },
+                    { new: true }
+                );
+            } else return res.json({ msg: "Movie already added to the liked list." });
+        } else await User.create({ email, likedMovies: [data] });
+        return res.json({ msg: "Movie successfully added to liked list." });
+    } catch (error) {
+        return res.json({ msg: "Error adding movie to the liked list" });
     }
 };
